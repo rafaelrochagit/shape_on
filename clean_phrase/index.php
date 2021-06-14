643749b15535c558c31d36f0aae457f2f2e0e141
Rafael Rocha <rafael.rocha@foton.la>
2021-06-13 22:31:23 -0300

    Mudança submit

--- clean_phrase/index.php
+++ clean_phrase/index.php
@@ -39,7 +39,7 @@
 			      	</div>
             	</div>
             	<div class="col-2">
-            		<button class="btn btn-primary" onclick="clean()">Limpar</button>
+            		<button class="btn btn-primary" onclick="clean()"> > </button>
             	</div>
             </div>
             
