f5496e4d8beb085aab25d5f920584b83cd062b7d
Rafael Rocha <rafael.rocha@foton.la>
2021-06-15 02:39:56 -0300

    Count Excluir

--- count/excluir.php
+++ count/excluir.php
@@ -1,12 +1,14 @@
 <?php
 require_once 'COUNT.php'; 
 session_start();
+
 $count = new COUNT();
+
 $simulacoesCount = $count->readSimulacoes();
 $codSimulacaoCountAtual = isset($_GET['codCount']) ? $_GET['codCount'] : null;
 
 if($codSimulacaoCountAtual != null) {
-	
+
     unset($simulacoesCount['count'.$codSimulacaoCountAtual]);
     $count->salvarSimulacoes($simulacoesCount);
     $_SESSION["msgSuccess"] = "Simulação ".$codSimulacaoCountAtual." excluída com sucesso!";
