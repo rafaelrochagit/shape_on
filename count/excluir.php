02f8278a30ed9d0faef1c38aea598301072874f5
Rafael Rocha <rafael.rocha@foton.la>
2021-06-15 02:33:11 -0300

    Count

--- count/excluir.php
+++ count/excluir.php
@@ -1,15 +1,15 @@
 <?php
-require_once 'MEI.php'; 
+require_once 'COUNT.php'; 
 session_start();
-$mei = new MEI();
-$simulacoesMei = $mei->readSimulacoes();
-$codSimulacaoMeiAtual = isset($_GET['codSimulacaoMei']) ? $_GET['codSimulacaoMei'] : null;
-if($codSimulacaoMeiAtual != null) {
-    unset($simulacoesMei['meiLiquido'.$codSimulacaoMeiAtual]);
-    $mei->salvarSimulacoes($simulacoesMei);
-    $_SESSION["msgSuccess"] = "Simulação ".$codSimulacaoMeiAtual." excluída com sucesso!";
+$count = new COUNT();
+$simulacoesCount = $count->readSimulacoes();
+$codSimulacaoCountAtual = isset($_GET['codCount']) ? $_GET['codCount'] : null;
+if($codSimulacaoCountAtual != null) {
+    unset($simulacoesCount['count'.$codSimulacaoCountAtual]);
+    $count->salvarSimulacoes($simulacoesCount);
+    $_SESSION["msgSuccess"] = "Simulação ".$codSimulacaoCountAtual." excluída com sucesso!";
 } else {
     $_SESSION["msg"] = "Simulação não encontrada";
 }
-header("Location: lista_simulacoes.php");
+header("Location: lista.php");
 ?>
